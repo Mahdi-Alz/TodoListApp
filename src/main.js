bc0336cede171c282cfb133957d5b6f983390cf3
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2025-09-06 17:23:44 +0330

    fixed task edit delete button

--- src/main.js
+++ src/main.js
@@ -3,25 +3,21 @@ const taskList = document.querySelector(".task-todos");
 const addTaskBtn = document.getElementById("add-task-btn");
 const addTaskForm = document.getElementById("add-task-form");
 
-if (addTaskBtn && addTaskForm) {
-    addTaskBtn.addEventListener("click", () => {
-        addTaskForm.classList.toggle("hidden");
-        addTaskBtn.classList.add("hidden");
-    });
-}
-
-// Select the toggle button and the dropdown menu
-const toggleButton = document.getElementById("edit-delete-toggle");
-const dropdownMenu = document.getElementById("menu-dropdpwn");
-
-// Toggle menu visibility when clicking the button
-toggleButton.addEventListener("click", (e) => {
-    e.stopPropagation(); // Prevent event from bubbling to document
-    dropdownMenu.style.display =
-        dropdownMenu.style.display === "flex" ? "none" : "flex";
+addTaskBtn.addEventListener("click", () => {
+    addTaskForm.classList.toggle("hidden");
 });
 
-// Hide menu if clicking anywhere else on the document
-document.addEventListener("click", () => {
-    dropdownMenu.style.display = "none";
+document.addEventListener("click", (e) => {
+    const editDeleteToggle = e.target.closest("#edit-delete-toggle");
+
+    if (editDeleteToggle) {
+        editDeleteToggle.nextElementSibling.classList.toggle("show");
+        return;
+    }
+
+    if (!e.target.closest("#edit-delete-dropdown")) {
+        document
+            .querySelectorAll("#edit-delete-dropdown.show")
+            .forEach((d) => d.classList.remove("show"));
+    }
 });
